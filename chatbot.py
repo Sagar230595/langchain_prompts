8b46977f82761dc2fdd0bf4136561521da54cc6e
Sagar <spadhiyar230595@gmail.com>
2026-06-28 23:15:01 +0530

    chatbot code complete

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,21 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful AI assistant."),
+]
 
 while True:
     user_input = input("Enter your question (or type 'exit' to quit): ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input.lower() == 'exit':
         break
     response = model.invoke(chat_history)
-    chat_history.append(response.content)
+    chat_history.append(AIMessage(content=response.content))
     print("AI Response:", response.content)
 
 print(chat_history)
